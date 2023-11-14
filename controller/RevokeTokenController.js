e209a00daafcc68b011d7e5b38af59dbcdd02ea2
nima-abdpour <nimaabdpoor@gmail.com>
2023-11-14 12:10:10 +0330

    check update status in revoke token

--- controller/RevokeTokenController.js
+++ controller/RevokeTokenController.js
@@ -4,8 +4,10 @@ const {knex} = require("../db/DataBaseInit");
 async function RevokeToken(router, db) {
     router.post("/:nid/revokeToken", async (context, next) => {
         try {
-            const revokeTokenResult = await updateTokenActivationState(knex, context.user.tokenId)
-            console.log(revokeTokenResult)
+            let revokeTokenResult = await updateTokenActivationState(knex, context.user.tokenId)
+            if (!revokeTokenResult || revokeTokenResult !== 1){
+                context.throw("Error in revoking token", 500)
+            }
             context.status = 200
             return context.body = {message: "token successfully revoked!"}
         } catch (err) {
